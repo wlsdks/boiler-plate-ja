4381c42f28929ec5f992d5e2ed8b0341ae38f658
choijinan <wlsdks12@naver.com>
2022-03-12 04:57:03 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
 
+const config = require('./config/key')
+
+const { User } = require("./models/User") 
+
+//application/x-www-form-urlencoded 를 분석해서 가져온다.
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 를 분석해서 가져온다.
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://root:1234@boilerplate.0q0sb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!!안녕하세요!'))
+
+
+
+app.get('/', (req, res) => res.send('새해복 많이 받으세용가링!!'))
+
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body) 
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    }) 
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
