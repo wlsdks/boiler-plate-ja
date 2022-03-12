514a47aaf3b159bddbca48518852860c5d909f33
choijinan <wlsdks12@naver.com>
2022-03-13 03:18:40 +0900

    react

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const config = require('./config/key')
-
-const { User } = require("./models/User") 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { auth } = require('./middleware/auth');
+const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded 를 분석해서 가져온다.
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,12 +22,13 @@ mongoose.connect(config.mongoURI)
 
 
 
+app.get('/', (req, res) => res.send('Hello World!!'))
 
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요~")
+})
 
-app.get('/', (req, res) => res.send('새해복 많이 받으세용가링!!'))
-
-
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -43,7 +44,7 @@ app.post('/register', (req, res) => {
     }) 
 })
 
-app.post('/login', (req, res) =>{
+app.post('/api/users/login', (req, res) =>{
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -71,5 +72,36 @@ app.post('/login', (req, res) =>{
         })
     })
 })
+
+
+app.get('/api/users/auth', auth , (req, res) => {
+
+    //여기까지 미들웨어를 통과해서 왔다는 얘기는 Authentication이 true라는 말이다.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+    User.findOneAndUpdate({_id: req.user._id},
+        {token: ""},
+        (err, user) =>{
+            if(err) return res.json({success: false, err});
+            return res.status(200).send({
+                success: true
+            })
+        })
+
+})
+
+
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
