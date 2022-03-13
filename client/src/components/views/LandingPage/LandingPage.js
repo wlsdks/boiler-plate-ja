73d1c5a7d41755273a60d99d5a26716e325ef64e
choijinan <wlsdks12@naver.com>
2022-03-13 21:14:08 +0900

    페이지 구성 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,36 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+    let navigate = useNavigate();
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
 
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success){
+                navigate('/login')
+            }else{
+                alert('로그아웃 하는데 실패 했습니다.')
+            }
+        })
+    }
+
     return(
-        <div>
-            랜딩 페이지입니다.
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+            }}>
+            <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
@@ -16,7 +16,7 @@ const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
 ReactDOM.render(
   <Provider
     store={createStoreWithMiddleware(Reducer, 
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
         window.__REDUX_DEVTOOLS_EXTENSION__()
       )}
   >
